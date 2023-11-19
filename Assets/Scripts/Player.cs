fc3cff882d148586127882ed349a4b86a5992135
NingzeSun <ningze1005@gmail.com>
2023-11-19 14:37:37 -0700

    .

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -139,7 +139,7 @@ public class Player : MonoBehaviour
         //Jumpng from the ground
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(0, jumpForce);
             jumpAudio.Play();
 
         }
@@ -148,13 +148,13 @@ public class Player : MonoBehaviour
             if (closeWithLeftWall)
             {
                 wallJumpCount--;
-                rb.velocity = new Vector2(0, jumpForce);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
                 jumpAudio.Play();
             }
             if (closeWithRightWall)
             {
                 wallJumpCount--;
-                rb.velocity = new Vector2(0, jumpForce);
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
                 jumpAudio.Play();
             }
         }
