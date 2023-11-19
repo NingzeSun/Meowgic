3bca00d2216186586151c490950d2f828cc07840
NingzeSun <ningze1005@gmail.com>
2023-11-19 09:38:04 -0700

    .

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -11,6 +11,7 @@ public class Death : StateMachineBehaviour
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        animator.Play("Death");
         GameObject.Find("DeathAudio").GetComponent<AudioSource>().Play();
     }
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
