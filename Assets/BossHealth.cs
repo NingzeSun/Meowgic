999d8cc5b3711b7cecbc08c0773f0c3c56d60d4a
Dian Huang <90299921+DiandyH@users.noreply.github.com>
2023-11-20 10:42:02 -0700

    boss sound hit

--- Assets/BossHealth.cs
+++ Assets/BossHealth.cs
@@ -8,20 +8,24 @@ public class BossHealth : MonoBehaviour
 {
 
 	public int health = 50;
-
+	
 	public GameObject deathEffect;
+	public AudioSource bossHitSound;
 
 	public bool isInvulnerable = false;
 
+	
+
 	public void TakeDamage(int damage)
-	{
+	{	
 		if (isInvulnerable)
 			return;
 		print("a");
 		health -= damage;
-
+		bossHitSound.Play();
 		if (health <= 25)
 		{
+			
 			GetComponent<Animator>().SetBool("IsEnraged", true);
 		}
 
@@ -31,6 +35,7 @@ public class BossHealth : MonoBehaviour
 			Invoke("LoadScene", 2.0f);
 		}
 	}
+
 
 	void LoadScene()
     {
